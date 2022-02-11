bd767c08cef9016b020fe73dd218680597992dfb
Hardews <2021214014@stu.cqupt.edu.cn>
2022-02-11 19:41:15 +0800

    fix(api):跨域中间件bug

--- middleware/cors.go
+++ middleware/cors.go
@@ -8,7 +8,7 @@ import (
 func Cors(c *gin.Context) {
 	method := c.Request.Method
 	if method != "" {
-		c.Header("Access-Control-Allow-Origin", c.GetHeader("origin")) // 可将将 * 替换为指定的域名
+		c.Header("Access-Control-Allow-Origin", "http://101.201.234.29")
 		c.Header("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE, UPDATE")
 		c.Header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
 		c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers, Cache-Control, Content-Language, Content-Type")
