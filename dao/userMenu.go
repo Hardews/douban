b974cf5a8fca7afdd346727bbb5e10879156d38c
Hardews <2021214014@stu.cqupt.edu.cn>
2022-03-31 08:49:45 +0800

    修改用户想看看过时无法保证数据一致性的bug

--- dao/userMenu.go
+++ dao/userMenu.go
@@ -2,242 +2,118 @@ package dao
 
 import (
 	"douban/model"
-	"strconv"
 )
 
-func GetWantSee(username string) (error, []model.UserHistory) {
-	var wantSees []model.UserHistory
-	sqlStr := "select comment,num,label from user_Want_See where username = ?"
-	stmt, err := dB.Prepare(sqlStr)
-	if err != nil {
-		return err, wantSees
+func GetWantSee(username string) (error, []model.UserWantSee) {
+	var wantSees []model.UserWantSee
+	TX := dB.Where("username = ?", username).Find(&[]model.UserWantSee{}).Scan(&wantSees)
+	if TX.Error != nil {
+		return TX.Error, wantSees
 	}
-	defer stmt.Close()
-
-	rows, err := stmt.Query(username)
-	if err != nil {
-		return err, wantSees
-	}
-	defer rows.Close()
-
-	sqlStr = "select img from movie_Base_Info where num = ?"
-	stmt, err = dB.Prepare(sqlStr)
-	if err != nil {
-		return err, wantSees
-	}
-
-	for rows.Next() {
-		var wantSee model.UserHistory
-		err := rows.Scan(&wantSee.Comment, &wantSee.MovieNum, &wantSee.Label)
-		if err != nil {
-			return err, wantSees
-		}
-
-		err = stmt.QueryRow(wantSee.MovieNum).Scan(&wantSee.Img)
-		if err != nil {
-			return err, wantSees
-		}
-
-		movieNum := strconv.Itoa(wantSee.MovieNum)
-		wantSee.Url = "http://49.235.99.195:8090/movieInfo/" + movieNum
-		wantSees = append(wantSees, wantSee)
-	}
-	return err, wantSees
+	return nil, wantSees
 }
 
-func GetSeen(username string) (error, []model.UserHistory) {
-	var Seens []model.UserHistory
-	sqlStr := "select comment,num,label from user_Seen where username = ?"
-	stmt, err := dB.Prepare(sqlStr)
-	if err != nil {
-		return err, Seens
+func GetSeen(username string) (error, []model.UserSeen) {
+	var Seen []model.UserSeen
+	TX := dB.Where("username = ?", username).Find(&[]model.UserSeen{}).Scan(&Seen)
+	if TX.Error != nil {
+		return TX.Error, Seen
 	}
-	defer stmt.Close()
-
-	rows, err := stmt.Query(username)
-	if err != nil {
-		return err, Seens
-	}
-	defer rows.Close()
-
-	sqlStr = "select img from movie_Base_Info where num = ?"
-	stmt, err = dB.Prepare(sqlStr)
-	if err != nil {
-		return err, Seens
-	}
-
-	for rows.Next() {
-		var Seen model.UserHistory
-		err := rows.Scan(&Seen.Comment, &Seen.MovieNum, &Seen.Label)
-		if err != nil {
-			return err, Seens
-		}
-		err = stmt.QueryRow(Seen.MovieNum).Scan(&Seen.Img)
-
-		movieNum := strconv.Itoa(Seen.MovieNum)
-		Seen.Url = "http://49.235.99.195:8090/movieInfo/" + movieNum
-		Seens = append(Seens, Seen)
-	}
-	return err, Seens
+	return nil, Seen
 }
 
-func UserSeen(username, comment, label string, movieNum int) error {
-	sqlStr := "insert user_Seen (username,comment,num,label) values (?,?,?,?)"
-	stmt, err := dB.Prepare(sqlStr)
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
+func UserSeen(seen model.UserSeen) error {
+	// 开启事务
+	tx := dB.Begin()
 
-	_, err = stmt.Exec(username, comment, movieNum, label)
-	if err != nil {
+	// 创建看过记录
+	tx.Create(&seen)
+	if err := tx.Error; err != nil {
+		tx.Rollback()
 		return err
 	}
 
-	sqlStr = "select Seen from movie_Extra_Info where num = ?"
-	stmt, err = dB.Prepare(sqlStr)
-	if err != nil {
-		return err
-	}
-	var num int
-	err = stmt.QueryRow(movieNum).Scan(&num)
-	if err != nil {
+	// 获取该电影的看过人数
+	var movieExtra model.MovieExtra
+	t := tx.Where("num = ?", seen.Num).Find(&model.MovieExtra{}).Scan(&movieExtra)
+	if err := t.Error; err != nil {
+		tx.Rollback()
 		return err
 	}
 
-	num += 1
-	sqlStr = "update movie_Extra_Info set Seen = ? where num = ?"
-	stmt, err = dB.Prepare(sqlStr)
-	if err != nil {
-		return err
+	// 人数加一并更新
+	movieExtra.SeenNum += 1
+	tX := tx.Model(&movieExtra).Update("seen_num = ?", movieExtra.SeenNum)
+	if tX.Error != nil {
+		tx.Rollback()
+		return tX.Error
 	}
-	_, err = stmt.Exec(num, movieNum)
-	if err != nil {
-		return err
-	}
-	return err
+
+	// 无错误提交
+	tx.Commit()
+	return nil
 }
 
-func UserWantSee(username, comment, label string, movieNum int) error {
-	sqlStr := "insert user_Want_See (username,comment,num,label) values (?,?,?,?)"
-	stmt, err := dB.Prepare(sqlStr)
-	if err != nil {
-		return err
-	}
-	defer stmt.Close()
-	_, err = stmt.Exec(username, comment, movieNum, label)
-	if err != nil {
+func UserWantSee(want model.UserWantSee) error {
+	// 开启事务
+	tx := dB.Begin()
+
+	// 创建想看记录
+	tx.Create(&want)
+	if err := tx.Error; err != nil {
+		// 出错回滚事务
+		tx.Rollback()
 		return err
 	}
 
-	sqlStr = "select wantSee from movie_Extra_Info where num = ?"
-	stmt, err = dB.Prepare(sqlStr)
-	if err != nil {
-		return err
-	}
-	var num int
-	err = stmt.QueryRow(movieNum).Scan(&num)
-	if err != nil {
+	// 获取该电影的想看人数
+	var movieExtra model.MovieExtra
+	t := tx.Where("num = ?", want.Num).Find(&model.MovieExtra{}).Scan(&movieExtra)
+	if err := t.Error; err != nil {
+		tx.Rollback()
 		return err
 	}
 
-	num += 1
-	sqlStr = "update movie_Extra_Info set wantSee = ? where num = ?"
-	stmt, err = dB.Prepare(sqlStr)
-	if err != nil {
-		return err
+	// 人数加一并更新
+	movieExtra.WantSeeNum += 1
+	tX := tx.Model(&movieExtra).Update("want_see_num = ?", movieExtra.WantSeeNum)
+	if tX.Error != nil {
+		tx.Rollback()
+		return tX.Error
 	}
-	_, err = stmt.Exec(num, movieNum)
-	if err != nil {
-		return err
-	}
-	return err
+
+	// 无错误提交
+	tx.Commit()
+	return nil
 }
 
-func GetUserComment(username string) (error, []model.UserComment, []model.UserComment) {
-	var shortComments, longComments []model.UserComment
-	sqlStr := "select movieNum,FilmCritics,time from short_Comment where username = ?"
-	stmt, err := dB.Prepare(sqlStr)
-	if err != nil {
-		return err, shortComments, longComments
+func GetUserComment(username string) (error, []model.ShortReview, []model.MovieReview) {
+	var short []model.ShortReview
+	var long []model.MovieReview
+	tx := dB.Where("username = ?", username).Find(&[]model.ShortReview{}).Scan(&short)
+	if tx.Error != nil {
+		return tx.Error, short, long
 	}
-	defer stmt.Close()
-	rows, err := stmt.Query(username)
-	if err != nil {
-		return err, shortComments, longComments
+	tx = dB.Where("username = ?", username).Find(&[]model.MovieReview{}).Scan(&long)
+	if tx.Error != nil {
+		return tx.Error, short, long
 	}
-
-	for rows.Next() {
-		var shortComment model.UserComment
-		err := rows.Scan(&shortComment.MovieNum, &shortComment.Txt, &shortComment.Time)
-		if err != nil {
-			return err, shortComments, longComments
-		}
-		movieNum := strconv.Itoa(shortComment.MovieNum)
-		shortComment.Url = "http://49.235.99.195:8090/movieInfo/" + movieNum
-		shortComments = append(shortComments, shortComment)
-	}
-
-	sqlStr = "select movieNum,Essay,time from long_Comment where username = ?"
-	stmt, err = dB.Prepare(sqlStr)
-	if err != nil {
-		return err, shortComments, longComments
-	}
-	rows, err = stmt.Query(username)
-	if err != nil {
-		return err, shortComments, longComments
-	}
-	defer rows.Close()
-
-	for rows.Next() {
-		var longComment model.UserComment
-		err := rows.Scan(&longComment.MovieNum, &longComment.Txt, &longComment.Time)
-		if err != nil {
-			return err, shortComments, longComments
-		}
-		movieNum := strconv.Itoa(longComment.MovieNum)
-		longComment.Url = "http://49.235.99.195:8090/movieInfo/" + movieNum
-		longComments = append(longComments, longComment)
-	}
-	return err, shortComments, longComments
+	return nil, short, long
 }
 
-func SetIntroduce(username, introduce string) error {
-	sqlStr := "update user_Menu set introduce = ? where username = ?"
-	stmt, err := dB.Prepare(sqlStr)
-	if err != nil {
-		return err
+func SetIntroduce(user model.UserMenu) error {
+	tx := dB.Model(&model.UserMenu{}).Where("username = ?", user.Username).Update("introduce", user.Introduce)
+	if tx.Error != nil {
+		return tx.Error
 	}
-	defer stmt.Close()
-	_, err = stmt.Exec(introduce, username)
-	if err != nil {
-		return err
-	}
-	return err
+	return nil
 }
 
-func UserMenuInfo(username string) (error, model.User) {
-	var user model.User
-	sqlStr := "select username,introduce from user_Menu where username = ?"
-	stmt, err := dB.Prepare(sqlStr)
-	if err != nil {
+func UserMenuInfo(username string) (error, model.UserMenu) {
+	var user model.UserMenu
+	tx := dB.Where("username = ?", username).First(&model.UserMenu{}).Scan(&user)
+	if err := tx.Error; err != nil {
 		return err, user
 	}
-	defer stmt.Close()
-	err = stmt.QueryRow(username).Scan(&username, &user.Introduce)
-	if err != nil {
-		return err, user
-	}
-
-	sqlStr = "select nickName,avatar,address from user_Base_Data where username = ?"
-	stmt, err = dB.Prepare(sqlStr)
-	if err != nil {
-		return err, user
-	}
-	err = stmt.QueryRow(username).Scan(&user.Nickname, &user.Img, &user.ImgAddress)
-	if err != nil {
-		return err, user
-	}
-
-	return err, user
+	return nil, user
 }
